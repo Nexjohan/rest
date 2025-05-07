e7695e57aa1dd73e8ec2cfde30c5ea10ccc6590d
Nexjohan <acadamia1110@gmail.com>
2025-05-07 16:38:22 -0500

    practica sin axios

--- src/main.js
+++ src/main.js
@@ -1,30 +1,21 @@
-const api = axios.create({
-  baseURL: 'https://api.themoviedb.org/3/',
-  headers: {
-    'Content-Type': 'application/json;charset=utf-8',
-  },
-  params: {
-    'api_key': API_KEY,
-  },
-});
+// Solo declara API_KEY una vez
 
 async function getTrendingMoviesPreview() {
-  const { data } = await api('trending/movie/day');
+  const res = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`);
+  const data = await res.json();
+
   const movies = data.results;
+  const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+  trendingPreviewMoviesContainer.innerHTML = ''; // Limpia contenido anterior
 
   movies.forEach(movie => {
-    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
-
     const movieContainer = document.createElement('div');
     movieContainer.classList.add('movie-container');
 
     const movieImg = document.createElement('img');
     movieImg.classList.add('movie-img');
     movieImg.setAttribute('alt', movie.title);
-    movieImg.setAttribute(
-      'src',
-      'https://image.tmdb.org/t/p/w300' + movie.poster_path,
-    );
+    movieImg.setAttribute('src', `https://image.tmdb.org/t/p/w300${movie.poster_path}`);
 
     movieContainer.appendChild(movieImg);
     trendingPreviewMoviesContainer.appendChild(movieContainer);
@@ -32,31 +23,34 @@ async function getTrendingMoviesPreview() {
 }
 
 async function getCategegoriesPreview() {
-  const { data } = await api('genre/movie/list');
+  const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
+  const data = await res.json();
+
   const categories = data.genres;
+  const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+  previewCategoriesContainer.innerHTML = ''; // Limpia contenido anterior
 
   categories.forEach(category => {
-    const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
-
     const categoryContainer = document.createElement('div');
     categoryContainer.classList.add('category-container');
 
     const categoryTitle = document.createElement('h3');
     categoryTitle.classList.add('category-title');
     categoryTitle.setAttribute('id', 'id' + category.id);
-    const categoryTitleText = document.createTextNode(category.name);
+    categoryTitle.textContent = category.name;
 
-    categoryTitle.appendChild(categoryTitleText);
     categoryContainer.appendChild(categoryTitle);
     previewCategoriesContainer.appendChild(categoryContainer);
   });
 }
 
 async function getRatedMoviesByGuestSession(guestSessionId) {
-  const { data } = await api(`guest_session/${guestSessionId}/rated/movies`);
-  const ratedMovies = data.results;
+  const res = await fetch(`https://api.themoviedb.org/3/guest_session/${guestSessionId}/rated/movies?api_key=${API_KEY}`);
+  const data = await res.json();
 
+  const ratedMovies = data.results;
   const ratedMoviesContainer = document.querySelector('#ratedMovies .movieList');
+  ratedMoviesContainer.innerHTML = ''; // Limpia contenido anterior
 
   ratedMovies.forEach(movie => {
     const movieContainer = document.createElement('div');
@@ -65,7 +59,7 @@ async function getRatedMoviesByGuestSession(guestSessionId) {
     const movieImg = document.createElement('img');
     movieImg.classList.add('movie-img');
     movieImg.setAttribute('alt', movie.title);
-    movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+    movieImg.setAttribute('src', `https://image.tmdb.org/t/p/w300${movie.poster_path}`);
 
     movieContainer.appendChild(movieImg);
     ratedMoviesContainer.appendChild(movieContainer);
